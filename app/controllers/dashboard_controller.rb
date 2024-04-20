72d6fc40c00253d5924b8a7f50640fa92c23f82d
abraao-junior <abraaojuniorsilva49@gmail.comm>
2024-04-19 22:03:37 -0300

    Implementação do dashboard

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,25 @@
+class DashboardController < ApplicationController
+  before_action :authenticate_user!
+  before_action :load_data, only: [:index]
+
+  def index
+    #Essas variáveis agora são carregadas pelo serviço no before_action
+  end
+
+  private
+
+  def load_data
+    # Inicializa o serviço DashboardService com o usuário atual
+    data_service = DashboardService.new(current_user)
+    data = data_service.load_data
+    @active_people_pie_chart = data[:active_people_pie_chart]
+    @total_debts = data[:total_debts]
+    @total_payments = data[:total_payments]
+    @balance = data[:balance] 
+    @last_debts = data[:last_debts]
+    @last_payments = data[:last_payments]
+    @my_people = data[:my_people]
+    @top_person = data[:top_person]
+    @bottom_person = data[:bottom_person]
+  end
+end
